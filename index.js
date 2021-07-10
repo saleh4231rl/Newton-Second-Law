d3b7908ff676e41c6814fd86663d669cac7af97b
saleh4231rl <52216220+saleh4231rl@users.noreply.github.com>
2021-07-10 20:52:29 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/** 
+  Algoritma Mencari Nilai Gaya
+
+1. Menentukan Nilai Masa
+2. Menentukan Nilai Percepatan
+3. Menghitung Sigma Gaya
+4. Mengalikan Masa dengan Percepatan
+5. Menampilkan NIlai dari Gaya
+
+Pseudocode
+STORE mass AS NUMBER WITH 100 Kg
+STORE acceleration AS NUMBER WITH 3 m/s2
+STORE Force AS NUMBER WITH 0
+
+CALCULATE  mass  TIMES  acceleration 
+SET  Force  VALUE WITH CALCULATION RESULT
+
+DISPLAY Force
+------------------------------------
+
+
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */ 
+
+
+
+// Insert your code here
+var massa;
+var percepatan;
